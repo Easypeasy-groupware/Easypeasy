49f4a61d1c6986d73e780a3336ac623762133b00
juwon0129 <115810020+juwon0129@users.noreply.github.com>
2023-03-09 18:11:26 +0900

    add : dataBoard 클래스 및 인터페이스 추가
    add : dataBoard 클래스 및 인터페이스 추가
    author : juwonchoi
    
    add com.ep.spring.dataBoard.controller.DataBoardController
    add com.ep.spring.dataBoard.model.dao.DataBoardDao
    add com.ep.spring.dataBoard.model.service.DataBoardService
    add com.ep.spring.dataBoard.model.service.DataBoardServiceImpl
    add com.ep.spring.dataBoard.model.vo.DataBoard
    
    add mappers/dataBoard-mapper
    
    add Controller, Dao, Service, ServiceImpl, vo, dataBaord-mapper.xml

---
+++ src/main/java/com/ep/spring/dataBoard/model/service/DataBoardService.java
@@ -0,0 +1,27 @@
+package com.ep.spring.dataBoard.model.service;
+
+import java.util.ArrayList;
+
+import com.ep.spring.common.model.vo.PageInfo;
+import com.ep.spring.dataBoard.model.vo.DataBoard;
+
+public interface DataBoardService {
+
+	// 1. 리스트 조회 (페이징 처리)
+	int selectListCount();
+	ArrayList<DataBoard> selectList(PageInfo pi);
+	
+	// 2. 게시글 작성
+	int insertDataBoard(DataBoard db);
+	
+	// 3. 게시글 상세조회
+	int increaseCount(int dbNo);
+	DataBoard selectDataBoard(int dbNo);
+	
+	// 4. 게시글 삭제
+	int deleteDataBoard(int dbNo);
+	
+	// 5. 게시글 수정
+	int updateDataBoard(DataBoard db);
+	
+}
